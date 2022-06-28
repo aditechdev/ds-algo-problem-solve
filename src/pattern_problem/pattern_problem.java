157801102708bd30ac76dc726b6e18d6beac68d3
aditechdev <aditechdevofficial@gmail.com>
2022-06-28 09:43:59 +0530

    :rocket: Half pyramid problem solve

--- src/pattern_problem/pattern_problem.java
+++ src/pattern_problem/pattern_problem.java
@@ -1,2 +1,70 @@
-package pattern_problem;public class pattern_problem {
+package pattern_problem;
+
+import java.util.Scanner;
+
+public class pattern_problem {
+    public static void main(String[] args) {
+  /*Half pyramid Problem
+  ***************************************
+        1
+        2 3
+        4 5 6
+        7 8 9 10
+  *****************************************
+   Observation
+   * We have N number of Rows
+   * Each Row has number of column equal to row number
+   * Value is incremented by 1
+   * co
+   *
+*/
+        int value = 1;
+
+        Scanner sc = new Scanner(System.in);
+        int numberOfRow = sc.nextInt();
+
+        for (int n = 1; n <= numberOfRow; n++) {
+            for (int m = 1; m <= n; m++) {
+                System.out.print(value + " ");
+                value++;
+            }
+            System.out.println();
+        }
+
+//        ***************************************
+//        *
+//        * *
+//        * * *
+//        * * * *
+//        *****************************************
+        for (int n = 1; n <= numberOfRow; n++) {
+            for (int m = 1; m <= n; m++) {
+                System.out.print("* ");
+
+            }
+            System.out.println();
+        }
+        for (int n = 1; n <= numberOfRow; n++) {
+            for (int m = 1; m <= n; m++) {
+                System.out.print(m+" ");
+            }
+            System.out.println();
+        }
+        char ch = 'a';
+
+        for (int n = 1; n <= numberOfRow; n++) {
+            for (int m = 1; m <= n; m++) {
+                System.out.print(ch+" ");
+                if (ch == 'z'){
+                    ch = 'a';
+                }else{
+                    ch++;
+
+                }
+
+
+            }
+            System.out.println();
+        }
+    }
 }
--- src/solve_pyramid.java
+++ src/solve_pyramid.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class solve_pyramid {
+import java.util.Scanner;
+
+public class solve_pyramid {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+   /*     Hour Glass Problem
+        1 2 3 4 5 6 7 8 9
+          1 2 4 5 6 7 8
+            1 2 3 4 5
+              1 2 3
+                1
+              1 2 3
+            1 2 3 4 5
+          1 2 3 4 5 6 7
+        1 2 3 4 5 6 7 8 9
+        spaces: o, 2, 4, 6, 8
+        
+    */
+
+        Scanner sc = new Scanner(System.in);
+        int myNumber = sc.nextInt();
+        for (int i = myNumber; i > 0; i--){
+
+            for (int j =  myNumber -i; j>=1; j-- ){
+                System.out.print("  ");
+            }
+            for (int k = 1; k <= i; k++){
+                System.out.print(k+" ");
+            }
+            System.out.println();
+
+        }
+    }
 }
