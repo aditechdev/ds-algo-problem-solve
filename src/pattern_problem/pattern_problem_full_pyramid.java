136f8f9ec112438737a4f6df368b5aab6b420ebe
aditechdev <aditechdevofficial@gmail.com>
2022-07-02 22:46:21 +0530

    :memo: AP

--- src/pattern_problem/pattern_problem_full_pyramid.java
+++ src/pattern_problem/pattern_problem_full_pyramid.java
@@ -4,6 +4,32 @@ import java.util.Scanner;
 
 public class pattern_problem_full_pyramid {
     public static void main(String[] args) {
+        /*
+        * Full Pyramid Problem
+        *
+        *      1        i=1
+        *     232       i-2
+        *    34553      1=3
+        *   4567654     1=4
+        *
+        * Observation:
+        * This is something called Arithmetic Progression
+        * -------------------------------------------------------
+        * Every row starts with the value equal to row number
+        * We have to print N number of ROWS
+        * Every row has N-i spaces, n number in increasing order
+        * followed by i-1 number in decreasing order
+        *
+        *
+        * For this we have used Airthematic Progression
+        * eg: 3,7,9,
+        * Ti = a + (i-1)d
+        * So,
+        *   = 3 +(i-1)2
+        *   = 3 + 2i - 2
+        *   = 1 + 2i
+        *   = 2i + 1
+        * */
 
 
 
