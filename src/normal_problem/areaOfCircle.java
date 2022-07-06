81a070a1490ed5e70ffabddfa8c6ce7c680a1364
aditechdev <aditechdevofficial@gmail.com>
2022-07-06 11:02:22 +0530

    :sparkles: Solve volume of sphere by Math.ceil& Math.pow

--- src/normal_problem/areaOfCircle.java
+++ src/normal_problem/areaOfCircle.java
@@ -3,17 +3,27 @@ package normal_problem;
 import java.util.Scanner;
 
 public class areaOfCircle {
-    public static int solve(int A) {
+    public static int solveAreaOfCircle(int A) {
 
         double areaOfCircle = Math.PI * A * A;
         int ab = (int) Math.ceil(areaOfCircle);
         return ab;
     }
+
+    public static int solveVolumeOfSphere(final int A) {
+
+        double volumeOfSphere = (4 * Math.PI * Math.pow(A,3))/3;
+        int ceil = (int) Math.ceil(volumeOfSphere);
+        return ceil;
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int radius = sc.nextInt();
 
-        int ab = solve(sc.nextInt());
-        System.out.println(ab);
+        int areaOfCircle = solveAreaOfCircle(radius);
+        System.out.println(areaOfCircle);
+        int volumeOfSphere = solveVolumeOfSphere(radius);
+        System.out.println(volumeOfSphere);
 
     }
 }
