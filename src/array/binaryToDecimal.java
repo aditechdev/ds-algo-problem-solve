1bbe626e2c0199bfcdcdf089efd62f54827e1ada
aditechdev <aditechdevofficial@gmail.com>
2022-07-10 17:39:01 +0530

    :sparkles: Decimal to binary

--- src/array/binaryToDecimal.java
+++ src/array/binaryToDecimal.java
@@ -14,9 +14,24 @@ public class binaryToDecimal {
         }
         return ans;
     }
+
+    public static int decimalToBinary(int N){
+        int ans = 0;
+        int power = 1;
+        while(N > 0){
+            int lastDigit = N%2;
+            ans = ans + lastDigit*power;
+            N = N/2;
+            power = power *10;
+        }
+        return ans;
+    }
+
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        System.out.println(binaryToDec(n));
+//        System.out.println(binaryToDec(n));
+        System.out.println(decimalToBinary(n));
     }
 }
