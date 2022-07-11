6eb0aff36b8f3e18057388776c5e5af969142158
aditechdev <aditechdevofficial@gmail.com>
2022-07-11 14:07:16 +0530

    :bug: 2d Array

--- src/array/twoDArray.java
+++ src/array/twoDArray.java
@@ -1,7 +1,22 @@
 package array;
 
-public class twoDArray {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class twoDArray {
+    // return index of x in the array, or return -1
+    static int linearSearch(int[] arr, int q){
+        for(int i = 0; i<arr.length; i++){
+            if (arr[i] == q){
+                return i;
+            }
+        }
+        return  -1;
+
+    }
+    public static void main(String[] args) {
+    int[] arr = {1, 2,6, 9, 19, 23, 48};
+        Scanner sc = new Scanner(System.in);
+        int key = sc.nextInt();
+        System.out.println(linearSearch(arr,key));
     }
 }
