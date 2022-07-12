65d8a10ccb0e81aa8fa1ea7d14f14eefc26ae9c8
aditechdev <aditechdevofficial@gmail.com>
2022-07-12 09:40:32 +0530

    :sparkles: First occurence

--- src/basic/array/twoDArray.java
+++ src/basic/array/twoDArray.java
@@ -55,6 +55,26 @@ public class twoDArray {
 //        element is not found
         return -1;
     }
+    static int binarySearchFirstOccurrence(int[] arr, int k){
+        int s = 0;
+        int e = arr.length-1;
+        int ans = -1; // assuming element doesn't exist but I will update once I get an occurrence
+        while (s<=e){
+            int mid = (s+e)/2;
+            if(arr[mid] > k){
+                e = mid -1;
+            } else if (arr[mid] <k) {
+                s = mid +1;
+
+            }else{
+                ans = mid;
+//                Update the answer and search left
+                e = mid -1;
+            }
+        }
+        return ans;
+
+    }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
