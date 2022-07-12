b672194a33f9312aa47a7f1301335c94e9c3a884
aditechdev <aditechdevofficial@gmail.com>
2022-07-12 11:36:58 +0530

    :sparkles: Last occurrence

--- src/basic/array/twoDArray.java
+++ src/basic/array/twoDArray.java
@@ -76,13 +76,37 @@ public class twoDArray {
 
     }
 
+    static int binarySearchLastOccurrence(int[] arr, int k){
+        int s = 0;
+        int e = arr.length-1;
+        int ans = -1; // assuming element doesn't exist but I will update once I get an occurrence
+        while (s<=e){
+            int mid = (s+e)/2;
+            if(arr[mid] > k){
+                e = mid -1;
+            } else if (arr[mid] <k) {
+                s = mid +1;
+
+            }else{
+                ans = mid;
+//                Update the answer and search left
+                s = mid + 1;
+            }
+        }
+        return ans;
+
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int key = sc.nextInt();
         int[] arr = {1, 2, 6, 9, 19, 23, 48};
         int[] arr2 = {1, 2, -1, -1, -3, 23, 48};
         int[] sortedArrayList = {1,2,5,7,9,13,24,34,39,54};
-        System.out.println(binarySearch(sortedArrayList,key));
+        int[] repeatElementArrayList = {1,2,4,8,11,11, 11, 11,7,9,13,24,34,39,54};
+        System.out.println(binarySearchFirstOccurrence(repeatElementArrayList,key));
+        System.out.println(binarySearchLastOccurrence(repeatElementArrayList,key));
+//        System.out.println(binarySearch(sortedArrayList,key));
 
 //        linearSearchAll(arr2, key);
 //        System.out.println(linearSearchAllActualApproach(arr2,key));
