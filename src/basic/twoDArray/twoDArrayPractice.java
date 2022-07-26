b2fae8a228ee164060b7f6bf77609ec6a9681e7d
aditechdev <aditechdevofficial@gmail.com>
2022-07-26 22:41:01 +0530

    :sparkles: 2d array display

--- src/basic/twoDArray/twoDArrayPractice.java
+++ src/basic/twoDArray/twoDArrayPractice.java
@@ -1,9 +1,9 @@
 package basic.twoDArray;
 
+import java.util.Scanner;
+
 public class twoDArrayPractice {
-    public static void main(String[] args) {
-//        Hardcoded Array
-//        jagged Array: Variable no of columns
+    static void hardCodedArray() {
         int[][] arr = {
                 {10, 20, 40, 59},
                 {4, 93, 23},
@@ -14,13 +14,44 @@ public class twoDArrayPractice {
         System.out.println(rows);
 
 //        print column
-        for (int i = 0; i< rows; i++){
+        for (int i = 0; i < rows; i++) {
             int column_in_each_rows = arr[i].length;
-            for (int j = 0; j<column_in_each_rows; j++){
-                System.out.print(arr[i][j]+", ");
+            for (int j = 0; j < column_in_each_rows; j++) {
+                System.out.print(arr[i][j] + ", ");
             }
             System.out.println();
+        }
+    }
+
+    static int[][] create2dArray(){
+        Scanner sc = new Scanner(System.in);
+        int row = sc.nextInt();
+        int column = sc.nextInt();
+        int[][] arr = new int[row][column];
+        for (int i = 0; i < row ; i++) {
+            for (int j = 0; j < column; j++) {
+                arr[i][j] = sc.nextInt();
+            }
 
         }
+        return arr;
+    }
+    static void displayArray(int[][] arr){
+        for (int i = 0; i < arr.length; i++) {
+            int column_in_each_rows = arr[i].length;
+            for (int j = 0; j < column_in_each_rows; j++) {
+                System.out.print(arr[i][j] + ", ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+//        Hardcoded Array
+//        hardCodedArray();
+        int[][] arr = create2dArray();
+//        jagged Array: Variable no of columns
+        displayArray(arr);
+
     }
 }
