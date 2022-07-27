fde945c713ffc3625ba71e36bc7c982b1d867710
aditechdev <aditechdevofficial@gmail.com>
2022-07-28 02:58:39 +0530

    :fire: Documentation done

---
+++ src/basic/string/StringBuilderDemo.java
@@ -0,0 +1,11 @@
+package basic.string;
+
+import java.util.Scanner;
+
+public class StringBuilderDemo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        System.out.println(s);
+    }
+}
