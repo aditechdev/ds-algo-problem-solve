10e5bd0781e417e2d711cbc9ef029ce87bd71878
aditechdev <aditechdevofficial@gmail.com>
2022-07-30 13:40:56 +0530

    :zap: OOPS added

---
+++ src/basic/oopsDemo/ProductDemo.java
@@ -0,0 +1,26 @@
+package basic.oopsDemo;
+
+// A java file can have one or more classes
+public class ProductDemo {
+    //    try to demonstrate a product
+    public static void main(String[] args) {
+        Product p1 = new Product();
+        // Setting the properties of class
+        p1.name = "Camera1x100";
+        p1.price = 1200;
+        p1.seller = "AKA Electronics";
+        System.out.println(p1.name);
+        System.out.println(p1.price);
+        System.out.println(p1.seller);
+
+
+    }
+
+}
+
+class Product{
+    String name;
+    int price;
+    String seller;
+
+}
--- src/basic/string/StringBuilderDemo.java
+++ src/basic/string/StringBuilderDemo.java
@@ -2,10 +2,16 @@ package basic.string;
 
 import java.util.Scanner;
 
+/**
+ * String Builder is 
+ */
 public class StringBuilderDemo {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();
         System.out.println(s);
+        StringBuilder stringBuilder = new StringBuilder(s);
+        System.out.println(stringBuilder.capacity()); // to know the capacity
+        System.out.println(stringBuilder.length());
     }
 }
